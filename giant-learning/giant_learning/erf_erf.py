b75d4e6b30587fa1b57a4ca52dc88fd2d01d67d5
lucpoisson <luca.pesce@epfl.ch>
2023-09-15 09:55:30 +0200

    fig 1 for giantstep

--- giant-learning/giant_learning/erf_erf.py
+++ giant-learning/giant_learning/erf_erf.py
@@ -2,24 +2,24 @@ import numpy as np
 from scipy.special import erf
 
 from .base import OverlapsBase
-from .cython_erf_erf import erf_updates, erf_error
+# from .cython_erf_erf import erf_updates, erf_error
 
 
 
 
-def _target(local_fields):
-    return np.mean(erf(local_fields/np.sqrt(2)),axis=-1)
-def _activation(x):
-    return erf(x/np.sqrt(2))
-def _activation_derivative(x):
-    return np.sqrt(2/np.pi) * np.exp(-x**2/2)
+# def _target(local_fields):
+#     return np.mean(erf(local_fields/np.sqrt(2)),axis=-1)
+# def _activation(x):
+#     return erf(x/np.sqrt(2))
+# def _activation_derivative(x):
+#     return np.sqrt(2/np.pi) * np.exp(-x**2/2)
 
-class ErfErfOverlaps(OverlapsBase):
-    def __init__(self, P: np.array, M0: np.array, Q0: np.array, a0: np.array, gamma0: float, d: int, l: int, noise: float, second_layer_update: bool, alpha: float):
-        super().__init__(_target, _activation, _activation_derivative, P, M0, Q0, a0, gamma0, d, l, noise, second_layer_update, alpha)
-        self.measure()
-    def compute_expected_values(self):
-        return erf_updates(self.Q, self.M, self.P, self.a, self.noise)
-    def measure(self):
-        self.test_errors.append(erf_error(self.Q, self.M, self.P, self.a, self.noise))
+# class ErfErfOverlaps(OverlapsBase):
+#     def __init__(self, P: np.array, M0: np.array, Q0: np.array, a0: np.array, gamma0: float, d: int, l: int, noise: float, second_layer_update: bool, alpha: float):
+#         super().__init__(_target, _activation, _activation_derivative, P, M0, Q0, a0, gamma0, d, l, noise, second_layer_update, alpha)
+#         self.measure()
+#     def compute_expected_values(self):
+#         return erf_updates(self.Q, self.M, self.P, self.a, self.noise)
+#     def measure(self):
+#         self.test_errors.append(erf_error(self.Q, self.M, self.P, self.a, self.noise))
         
--- reproduce_giant.py
+++ reproduce_giant.py
@@ -1,6 +1,5 @@
 from giant_learning.gradient_descent import GradientDescent
 from giant_learning.montecarlo_overlaps import MonteCarloOverlaps
-from giant_learning.erf_erf import ErfErfOverlaps
 
 import numpy as np
 from sklearn.preprocessing import normalize
@@ -20,75 +19,10 @@ def target(lft):
     if target_tkn == '100':
         return lft[...,0]/3 + 2*lft[...,0]*H_2(lft[...,1]) + lft[...,1]*lft[...,2]
 
-# 3D plot T similarity matrices, last dimension are the 3 coordinates of the points
-def twoDplot_at(t):
-    # ion on the planes xy and xz
-    fig, ax = plt.subplots(1,2)
-    ax[0].scatter(
-        similarity_simulation[t,:,0],
-        similarity_simulation[t,:,1],
-        color = 'orange',
-        label = 'GD'
-    )
-    ax[1].scatter(
-        similarity_simulation[t,:,2],
-        similarity_simulation[t,:,1],
-        color = 'orange',
-        label = 'GD'
-    )
-    ax[0].set_xlim(-1,1) ; ax[0].set_ylim(-1,1)
-    ax[1].set_xlim(-1,1) ; ax[1].set_ylim(-1,1)
-
-    circle00 = plt.Circle((0, 0), 1, color='black', fill=False)
-    circle10 = plt.Circle((0, 0), 1, color='black', fill=False)
-
-
-    ax[0].add_artist(circle00)
-    ax[1].add_artist(circle10)
-
-    ax[0].legend()
-    ax[1].legend()
-
-    plt.show()
-
-def riskplot():
-    # plot risk of GD and MC and GD no resample
-    fig, ax = plt.subplots(1,1)
-    ax.plot(np.arange(T+1), simulation.test_errors, label='GD', marker='o', color='orange')
-    ax.plot(np.arange(T+1), simulation_noresample.test_errors, label='GD no resample', marker='o', color='green')
-    ax.plot(np.arange(T+1), montecarlo.test_errors, label='MC', marker='o', color='blue')
-    ax.set_xlabel('t') 
-    ax.set_ylabel('Test Risk')
-    ax.legend()
-    plt.show()
-
-def threeDplot_at(t):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    # plot the unit sphere
-    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-    x = np.cos(u)*np.sin(v)
-    y = np.sin(u)*np.sin(v)
-    z = np.cos(v)
-    ax.plot_wireframe(x, y, z, color='black',alpha=0.1)
-
-    ax.set_xlim(-1,1) ; ax.set_ylim(-1,1) ; ax.set_zlim(-1,1)
-
-    ax.scatter(
-        similarity_simulation[t,:,0],
-        similarity_simulation[t,:,1],
-        similarity_simulation[t,:,2],
-        color = 'orange',
-        label='GD'
-    )
-
-    ax.legend()
-
-    plt.show()
 second_layers = {'gaussian': 1/np.sqrt(p)*np.random.randn(p) , 'hypercube': np.sign(np.random.normal(size=(p,))) /np.sqrt(p) , 'ones': np.ones(p)/np.sqrt(p)}
 
 tkns = ['111','120','100']
-ds = [10,20,40] 
+ds = [1000,2000,4000] 
 choices = ['gaussian', 'hypercube', 'ones']
 for d in ds: 
     for target_tkn in tkns:
@@ -133,10 +67,8 @@ for d in ds:
                 1/np.sqrt(np.diag(P))
             )
 
-            
-            # for t in range(T):
-            #     twoDplot_at(t)
-            #     threeDplot_at(t)
             t1_stop = perf_counter_ns()
             print("Elapsed time:", (t1_stop - t1_start)*1e-9, 's')
             np.savez(f'./results_reproduce/data_d={d}_tkn={target_tkn}_choice2={choice_2layer}.npz',similarity_simulation)
+
+            
