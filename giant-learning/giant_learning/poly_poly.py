7fdc10a6881bd44bf97ef45b97aafc4f4c3c7c29
Luca Arnaboldi <luca@arnaboldi.lu>
2023-10-31 15:22:10 +0100

    Fixed cubic

--- giant-learning/giant_learning/poly_poly.py
+++ giant-learning/giant_learning/poly_poly.py
@@ -123,38 +123,38 @@ class H3H3Overlaps(PolynomialPolynomialOverlapsBase):
     
     @staticmethod
     def _activation(x):
-        return x**3 - x
+        return x**3 - 3*x
     
     @staticmethod
     def _activation_derivative(x):
-        return 3 * x**2 - 1
+        return 3 * x**2 - 3
 
     @staticmethod
     def _I2noise(Caa,Cab,Cbb):
-        return 1 - 3*Caa + 18*Cab**2 - 3*Cbb + 9*Caa*Cbb
+        return 9 - 9*Caa + 18*Cab**2 - 9*Cbb + 9*Caa*Cbb
 
     @staticmethod
     def _I2(Caa, Cab, Cbb):
-        return Cab - 3*Caa*Cab + 6*Cab**3 - 3*Cab*Cbb + 9*Caa*Cab*Cbb
+        return 9*Cab - 9*Caa*Cab + 6*Cab**3 - 9*Cab*Cbb + 9*Caa*Cab*Cbb
 
     @staticmethod
     def _I3(Caa, Cab, Cac, Cbb, Cbc, Ccc):
-        return -6*Cab*Cac + Cbc - 3*Caa*Cbc + 18*Cac**2*Cbc + 18*Cab*Cac*Ccc - 3*Cbc*Ccc + 9*Caa*Cbc*Ccc
+        return -18*Cab*Cac + 9*Cbc - 9*Caa*Cbc + 18*Cac**2*Cbc + 18*Cab*Cac*Ccc - 9*Cbc*Ccc + 9*Caa*Cbc*Ccc
 
     @staticmethod
     def _I4(Caa, Cab, Cac, Cad, Cbb, Cbc, Cbd, Ccc, Ccd, Cdd):
         return (
-            -6*Cac*Cad + 18*Cac*Cad*Cbb + 36*Cab*Cad*Cbc - 108*Cac*Cad*Cbc**2 + 36*Cab*Cac*Cbd - 6*Cbc*Cbd + 18*Caa*Cbc*Cbd - 108*Cac**2*Cbc*Cbd - 
-            108*Cad**2*Cbc*Cbd - 108*Cac*Cad*Cbd**2 + 18*Cac*Cad*Ccc - 54*Cac*Cad*Cbb*Ccc - 108*Cab*Cad*Cbc*Ccc - 108*Cab*Cac*Cbd*Ccc + 18*Cbc*Cbd*Ccc - 
-            54*Caa*Cbc*Cbd*Ccc + 324*Cad**2*Cbc*Cbd*Ccc + 324*Cac*Cad*Cbd**2*Ccc + Ccd - 3*Caa*Ccd + 18*Cab**2*Ccd + 18*Cac**2*Ccd + 18*Cad**2*Ccd - 3*Cbb*Ccd + 
-            9*Caa*Cbb*Ccd - 54*Cac**2*Cbb*Ccd - 54*Cad**2*Cbb*Ccd - 216*Cab*Cac*Cbc*Ccd + 18*Cbc**2*Ccd - 54*Caa*Cbc**2*Ccd + 324*Cad**2*Cbc**2*Ccd - 
-            216*Cab*Cad*Cbd*Ccd + 1296*Cac*Cad*Cbc*Cbd*Ccd + 18*Cbd**2*Ccd - 54*Caa*Cbd**2*Ccd + 324*Cac**2*Cbd**2*Ccd - 3*Ccc*Ccd + 9*Caa*Ccc*Ccd - 
-            54*Cab**2*Ccc*Ccd - 54*Cad**2*Ccc*Ccd + 9*Cbb*Ccc*Ccd - 27*Caa*Cbb*Ccc*Ccd + 162*Cad**2*Cbb*Ccc*Ccd + 648*Cab*Cad*Cbd*Ccc*Ccd - 54*Cbd**2*Ccc*Ccd + 
-            162*Caa*Cbd**2*Ccc*Ccd - 108*Cac*Cad*Ccd**2 + 324*Cac*Cad*Cbb*Ccd**2 + 648*Cab*Cad*Cbc*Ccd**2 + 648*Cab*Cac*Cbd*Ccd**2 - 108*Cbc*Cbd*Ccd**2 + 
-            324*Caa*Cbc*Cbd*Ccd**2 + 6*Ccd**3 - 18*Caa*Ccd**3 + 108*Cab**2*Ccd**3 - 18*Cbb*Ccd**3 + 54*Caa*Cbb*Ccd**3 + 18*Cac*Cad*Cdd - 54*Cac*Cad*Cbb*Cdd - 
-            108*Cab*Cad*Cbc*Cdd + 324*Cac*Cad*Cbc**2*Cdd - 108*Cab*Cac*Cbd*Cdd + 18*Cbc*Cbd*Cdd - 54*Caa*Cbc*Cbd*Cdd + 324*Cac**2*Cbc*Cbd*Cdd - 
-            54*Cac*Cad*Ccc*Cdd + 162*Cac*Cad*Cbb*Ccc*Cdd + 324*Cab*Cad*Cbc*Ccc*Cdd + 324*Cab*Cac*Cbd*Ccc*Cdd - 54*Cbc*Cbd*Ccc*Cdd + 
-            162*Caa*Cbc*Cbd*Ccc*Cdd - 3*Ccd*Cdd + 9*Caa*Ccd*Cdd - 54*Cab**2*Ccd*Cdd - 54*Cac**2*Ccd*Cdd + 9*Cbb*Ccd*Cdd - 27*Caa*Cbb*Ccd*Cdd + 
-            162*Cac**2*Cbb*Ccd*Cdd + 648*Cab*Cac*Cbc*Ccd*Cdd - 54*Cbc**2*Ccd*Cdd + 162*Caa*Cbc**2*Ccd*Cdd + 9*Ccc*Ccd*Cdd - 27*Caa*Ccc*Ccd*Cdd + 
-            162*Cab**2*Ccc*Ccd*Cdd - 27*Cbb*Ccc*Ccd*Cdd + 81*Caa*Cbb*Ccc*Ccd*Cdd
+            -162*Cac*Cad + 162*Cac*Cad*Cbb + 324*Cab*Cad*Cbc - 324*Cac*Cad*Cbc**2 + 324*Cab*Cac*Cbd - 162*Cbc*Cbd + 162*Caa*Cbc*Cbd - 324*Cac**2*Cbc*Cbd - 
+            324*Cad**2*Cbc*Cbd - 324*Cac*Cad*Cbd**2 + 162*Cac*Cad*Ccc - 162*Cac*Cad*Cbb*Ccc - 324*Cab*Cad*Cbc*Ccc - 324*Cab*Cac*Cbd*Ccc + 162*Cbc*Cbd*Ccc - 
+            162*Caa*Cbc*Cbd*Ccc + 324*Cad**2*Cbc*Cbd*Ccc + 324*Cac*Cad*Cbd**2*Ccc + 81*Ccd - 81*Caa*Ccd + 162*Cab**2*Ccd + 162*Cac**2*Ccd + 162*Cad**2*Ccd - 
+            81*Cbb*Ccd + 81*Caa*Cbb*Ccd - 162*Cac**2*Cbb*Ccd - 162*Cad**2*Cbb*Ccd - 648*Cab*Cac*Cbc*Ccd + 162*Cbc**2*Ccd - 162*Caa*Cbc**2*Ccd + 
+            324*Cad**2*Cbc**2*Ccd - 648*Cab*Cad*Cbd*Ccd + 1296*Cac*Cad*Cbc*Cbd*Ccd + 162*Cbd**2*Ccd - 162*Caa*Cbd**2*Ccd + 324*Cac**2*Cbd**2*Ccd - 81*Ccc*Ccd + 
+            81*Caa*Ccc*Ccd - 162*Cab**2*Ccc*Ccd - 162*Cad**2*Ccc*Ccd + 81*Cbb*Ccc*Ccd - 81*Caa*Cbb*Ccc*Ccd + 162*Cad**2*Cbb*Ccc*Ccd + 648*Cab*Cad*Cbd*Ccc*Ccd - 
+            162*Cbd**2*Ccc*Ccd + 162*Caa*Cbd**2*Ccc*Ccd - 324*Cac*Cad*Ccd**2 + 324*Cac*Cad*Cbb*Ccd**2 + 648*Cab*Cad*Cbc*Ccd**2 + 648*Cab*Cac*Cbd*Ccd**2 - 
+            324*Cbc*Cbd*Ccd**2 + 324*Caa*Cbc*Cbd*Ccd**2 + 54*Ccd**3 - 54*Caa*Ccd**3 + 108*Cab**2*Ccd**3 - 54*Cbb*Ccd**3 + 54*Caa*Cbb*Ccd**3 + 162*Cac*Cad*Cdd - 
+            162*Cac*Cad*Cbb*Cdd - 324*Cab*Cad*Cbc*Cdd + 324*Cac*Cad*Cbc**2*Cdd - 324*Cab*Cac*Cbd*Cdd + 162*Cbc*Cbd*Cdd - 162*Caa*Cbc*Cbd*Cdd + 
+            324*Cac**2*Cbc*Cbd*Cdd - 162*Cac*Cad*Ccc*Cdd + 162*Cac*Cad*Cbb*Ccc*Cdd + 324*Cab*Cad*Cbc*Ccc*Cdd + 324*Cab*Cac*Cbd*Ccc*Cdd - 
+            162*Cbc*Cbd*Ccc*Cdd + 162*Caa*Cbc*Cbd*Ccc*Cdd - 81*Ccd*Cdd + 81*Caa*Ccd*Cdd - 162*Cab**2*Ccd*Cdd - 162*Cac**2*Ccd*Cdd + 81*Cbb*Ccd*Cdd - 
+            81*Caa*Cbb*Ccd*Cdd + 162*Cac**2*Cbb*Ccd*Cdd + 648*Cab*Cac*Cbc*Ccd*Cdd - 162*Cbc**2*Ccd*Cdd + 162*Caa*Cbc**2*Ccd*Cdd + 81*Ccc*Ccd*Cdd - 
+            81*Caa*Ccc*Ccd*Cdd + 162*Cab**2*Ccc*Ccd*Cdd - 81*Cbb*Ccc*Ccd*Cdd + 81*Caa*Cbb*Ccc*Ccd*Cdd
         )
