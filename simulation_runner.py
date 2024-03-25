24581f43faa350b79ee5994ac12264ef52867d7e
Luca Arnaboldi <luca@arnaboldi.lu>
2024-03-25 17:35:45 +0100

    Simulation runner

--- simulation_runner.py
+++ simulation_runner.py
@@ -1,6 +1,6 @@
 from giant_learning.gradient_descent import SphericalGradientDescent, ProjectedGradientDescent, GradientDescent
 from giant_learning.montecarlo_overlaps import MonteCarloOverlaps
-from giant_learning.poly_poly import ProjectedH2H2Overlaps, H2H2Overlaps
+from giant_learning.poly_poly import ProjectedH3H3Overlaps, H3H3Overlaps
 
 import numpy as np
 from scipy.special import erf
@@ -11,21 +11,21 @@ import matplotlib.pyplot as plt
 run = True
 show = True
 
-mu = 0.
-delta = 3/2.
-T = 500
+mu = 1.7
+delta = -.15
+T = 2000
 nseeds = 1
-ds = np.logspace(8-1,10-1,base=2,num=3,dtype=int)
+ds = np.logspace(8,10,base=3,num=1,dtype=int)
 
 p = 1
 k = 1
 noise = 0.
 predictor_interaction = False
-target = H2H2Overlaps._target
-activation = H2H2Overlaps._activation
-activation_derivative = H2H2Overlaps._activation_derivative
+target = H3H3Overlaps._target
+activation = H3H3Overlaps._activation
+activation_derivative = H3H3Overlaps._activation_derivative
 
-filename = f'computation-database/H2H2_d{ds}_nseeds{nseeds}_T{T}_l{delta}_mu{mu}_k{k}.npz'.replace(' ',',')
+filename = f'computation-database/H3H3_d{ds}_nseeds{nseeds}_T{T}_l{delta}_mu{mu}_k{k}.npz'.replace(' ',',')
 
 ### RUN SIMULATIONS ###
 if run:
@@ -36,6 +36,8 @@ if run:
     spherical_gd_plain_errors = np.zeros((nseeds, len(ds), T+1))
     spherical_gd_correlation_errors = np.zeros((nseeds, len(ds), T+1))
 
+    gd_plain_m = np.zeros((nseeds, len(ds), T+1, k))
+    gd_correlation_m = np.zeros((nseeds, len(ds), T+1, k))
     projected_gd_plain_m = np.zeros((nseeds, len(ds), T+1, k))
     projected_gd_correlation_m = np.zeros((nseeds, len(ds), T+1, k))
     spherical_gd_plain_m = np.zeros((nseeds, len(ds), T+1, k))
@@ -45,7 +47,6 @@ if run:
     for i,d in enumerate(ds):
         n = int(np.power(d,mu))
         t = 1/np.sqrt(d)  ### fix initial overlap
-        t = 1e-10
         gamma = np.power(d,-delta)
         print(f'NOW Running d = {d}')
         for seed in range(nseeds):
@@ -77,54 +78,59 @@ if run:
                 target, Wtarget, n,
                 activation, W0, a0, activation_derivative,
                 gamma, noise, predictor_interaction=True,
-                test_size = None, analytical_error= 'H2H2'
+                test_size = None, analytical_error= 'H3H3'
             )
             gd_correlation = GradientDescent(
                 target, Wtarget, n,
                 activation, W0, a0, activation_derivative,
                 gamma, noise, predictor_interaction=False,
-                test_size = None, analytical_error= 'H2H2'
+                test_size = None, analytical_error= 'H3H3'
             )
             spherical_gd_plain = SphericalGradientDescent(
                 target, Wtarget, n,
                 activation, W0, a0, activation_derivative,
                 gamma, noise, predictor_interaction=True,
-                test_size = None, analytical_error= 'H2H2'
+                test_size = None, analytical_error= 'H3H3'
             )
             spherical_gd_correlation = SphericalGradientDescent(
                 target, Wtarget, n,
                 activation, W0, a0, activation_derivative,
                 gamma, noise, predictor_interaction=False,
-                test_size = None, analytical_error= 'H2H2'
+                test_size = None, analytical_error= 'H3H3'
             )
             projected_gd_plain = ProjectedGradientDescent(
                 target, Wtarget, n,
                 activation, W0, a0, activation_derivative,
                 gamma, noise, predictor_interaction=True,
-                test_size = None, analytical_error= 'H2H2'
+                test_size = None, analytical_error= 'H3H3'
             )
             projected_gd_correlation = ProjectedGradientDescent(
                 target, Wtarget, n,
                 activation, W0, a0, activation_derivative,
                 gamma, noise, predictor_interaction=False,
-                test_size = None, analytical_error= 'H2H2'
+                test_size = None, analytical_error= 'H3H3'
             )
 
-            spherical_gd_plain.train(T)
+
+            spherical_gd_plain.train(T, verbose=True)
             print('Trained spherical_gd_plain')
-            spherical_gd_correlation.train(T)
+            spherical_gd_correlation.train(T, verbose=True)
             print('Trained spherical_gd_correlation')
-            projected_gd_plain.train(T)
+            projected_gd_plain.train(T, verbose=True)
             print('Trained projected_gd_plain')
-            projected_gd_correlation.train(T)
+            projected_gd_correlation.train(T, verbose=True)
             print('Trained projected_gd_correlation')
             
 
+            # gd_plain_errors[seed, i, :] = gd_plain.test_errors
+            # gd_correlation_errors[seed, i, :] = gd_correlation.test_errors
             projected_gd_plain_errors[seed, i, :] = projected_gd_plain.test_errors
             projected_gd_correlation_errors[seed, i, :] = projected_gd_correlation.test_errors
             spherical_gd_plain_errors[seed, i, :] = spherical_gd_plain.test_errors
             spherical_gd_correlation_errors[seed, i, :] = spherical_gd_correlation.test_errors
 
+            # gd_plain_m[seed, i, :] = np.einsum('tja,ra->tjr', gd_plain.W_s, Wtarget).reshape(T+1,1)
+            # gd_correlation_m[seed, i, :] = np.einsum('tja,ra->tjr', gd_correlation.W_s, Wtarget).reshape(T+1,1)
             projected_gd_plain_m[seed, i, :] = np.einsum('tja,ra->tjr', projected_gd_plain.W_s, Wtarget).reshape(T+1,1)
             projected_gd_correlation_m[seed, i, :] = np.einsum('tja,ra->tjr', projected_gd_correlation.W_s, Wtarget).reshape(T+1,1)
             spherical_gd_plain_m[seed, i, :] = np.einsum('tja,ra->tjr', spherical_gd_plain.W_s, Wtarget).reshape(T+1,1)
@@ -138,12 +144,19 @@ if run:
         projected_gd_plain_errors=projected_gd_plain_errors,
         projected_gd_correlation_errors=projected_gd_correlation_errors,
         spherical_gd_plain_errors=spherical_gd_plain_errors,
-        spherical_gd_correlation_errors=spherical_gd_correlation_errors
+        spherical_gd_correlation_errors=spherical_gd_correlation_errors,
+        #
+        gd_plain_m=gd_plain_m,
+        gd_correlation_m=gd_correlation_m,
+        projected_gd_plain_m=projected_gd_plain_m,
+        projected_gd_correlation_m=projected_gd_correlation_m,
+        spherical_gd_plain_m=spherical_gd_plain_m,
+        spherical_gd_correlation_m=spherical_gd_correlation_m
     )
 
 
 ######## PLOTS ########
-# Load data
+## Load data
 data = np.load(filename)
 ds = data['ds']
 gd_plain_errors = data['gd_plain_errors']
@@ -152,6 +165,13 @@ projected_gd_plain_errors = data['projected_gd_plain_errors']
 projected_gd_correlation_errors = data['projected_gd_correlation_errors']
 spherical_gd_plain_errors = data['spherical_gd_plain_errors']
 spherical_gd_correlation_errors = data['spherical_gd_correlation_errors']
+#
+gd_plain_m = data['gd_plain_m']
+gd_correlation_m = data['gd_correlation_m']
+projected_gd_plain_m = data['projected_gd_plain_m']
+projected_gd_correlation_m = data['projected_gd_correlation_m']
+spherical_gd_plain_m = data['spherical_gd_plain_m']
+spherical_gd_correlation_m = data['spherical_gd_correlation_m']
 
 
 if show:
